4e1d1f2c989ae68af9e98455336a30b0251f0e14
estebanix <dakisunshine@gmail.com>
2023-08-28 17:27:17 +0200

    setting new canvas

--- src/Files/Canvas.tsx
+++ src/Files/Canvas.tsx
@@ -1,80 +1,19 @@
-import {useState} from "react";
-import {Scatterplot} from "./Scatterplot";
-import { data } from "../Datas/data";
+import ScatterPlot from "./ScatterPlot";
 
-interface Option {
-    id: number;
-    value: string;
-    label : string;
-}
-
-interface Dating {
-    id: number;
-    value: string;
-    label : string;
-}
-
-interface Dropdown{
-    options: Option[];
-    id: string;
-    selectedValue: string;
-    onSelectedValueChange: (value: string) => void;
-}
-
-const Dropdown: React.FC<Dropdown> = ({ options, id, selectedValue, onSelectedValueChange }) => (
-  <select id={id} onChange={event => onSelectedValueChange(event.target.value)}>
-    {options.map(({ value, label }) => (
-      <option value={value} selected={value === selectedValue}>
-        {label}
-      </option>
-    ))}
-  </select>
-);
-
- const options: Option[] = [
-    { id: 1, value: 'all', label: 'All Continents' },
-    { id: 2, value: 'americas', label: 'Americas' },
-    { id: 3, value: 'africa', label: 'Africa' },
-    { id: 4, value: 'europe', label: 'Europe' },
-    { id: 5, value: 'asia', label: 'Asia' },
-    { id: 6, value: 'oceania', label: 'Oceania' }
+const Canvas = () => {
+  const data = [
+    { x: 10, y: 20 },
+    { x: 20, y: 30 },
+    { x: 30, y: 10 },
+    // Add more data points
   ];
-  const initialOption = 'all';
 
-  const dating: Dating[] = [
-    {id: 1, value: 'all', label: 'All'},
-    {id: 2, value: 'small', label: 'Small'},
-    {id: 3, value: 'medium', label: 'Medium'},
-    {id: 4, value: 'large', label: 'Large'}
-  ];
-  const initialDate = 'all';
-  
-  export default function Canvas () {
-    const [selectedValue, setSelectedValue] = useState(initialOption);
-    const [selectedDate, setSelectedDate] = useState(initialDate);
+  return (
+    <div className="App">
+      <h1>Scatter Plot Example</h1>
+      <ScatterPlot data={data} width={500} height={300} />
+    </div>
+  );
+};
 
-    const currentImage = options.find(dat => dat.value === selectedValue);
-    const currentDate = dating.find(dat => dat.value === selectedDate);
-    
-    return (
-      <div className="canvas--container">
-        <label for="pet-select">Choose a continent:</label>
-        <div className="select--box">
-          <Dropdown
-            options={options}
-            id="continent-select"
-            selectedValue={selectedValue}
-            onSelectedValueChange={setSelectedValue}
-          />
-          <Dropdown
-            options={dating}
-            id="dating-select"
-            selectedValue={selectedDate}
-            onSelectedValueChange={setSelectedDate}
-          />
-        </div>
-        <input type="range" />
-        <Scatterplot data={data} width={400} height={400} state={currentImage?.value} dating={currentDate?.value}/>
-      </div>
-    );
-  }
+export default Canvas;
--- src/Files/Scatterplot.tsx
+++ src/Files/Scatterplot.tsx
@@ -1,131 +1,50 @@
+import { useRef, useEffect } from "react";
 import * as d3 from "d3";
-import { AxisLeft } from "./AxisLeft";
-import { AxisBottom } from "./AxisBottom";
-import { useState } from "react";
-import { InteractionData, Tooltip } from "./Tooltip";
 
-const MARGIN = { top: 60, right: 60, bottom: 60, left: 60 };
+const ScatterPlot = ({ data, width, height }) => {
+  const svgRef = useRef();
 
-type DataPoint = {
-  x: number;
-  y: number;
-  size: number;
-  group: string;
-  subGroup: string;
+  useEffect(() => {
+    // Clear any previous content
+    d3.select(svgRef.current).selectAll("*").remove();
+
+    // Create an SVG container
+    const svg = d3.select(svgRef.current)
+      .attr("width", width)
+      .attr("height", height);
+
+    // Create scales for x and y dimensions
+    const xScale = d3.scaleLinear()
+      .domain(d3.extent(data, d => d.x))
+      .range([0, width]);
+
+    const yScale = d3.scaleLinear()
+      .domain(d3.extent(data, d => d.y))
+      .range([height, 0]);
+
+    // Create x-axis and y-axis
+    const xAxis = d3.axisBottom(xScale);
+    const yAxis = d3.axisLeft(yScale);
+
+    svg.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(xAxis);
+
+    svg.append("g")
+      .call(yAxis);
+
+    // Create circles for each data point
+    svg.selectAll("circle")
+      .data(data)
+      .enter()
+      .append("circle")
+      .attr("cx", d => xScale(d.x))
+      .attr("cy", d => yScale(d.y))
+      .attr("r", 5)
+      .attr("fill", "steelblue");
+  }, [data, width, height]);
+
+  return <svg ref={svgRef}></svg>;
 };
 
-type ScatterplotProps = {
-  width: number;
-  height: number;
-  data: DataPoint[];
-  state: object;
-  dating: object;
-};
-
-export const Scatterplot = ({ width, height, data, state, dating }: ScatterplotProps) => {
-  
-  const boundsWidth = width - MARGIN.right - MARGIN.left;
-  const boundsHeight = height - MARGIN.top - MARGIN.bottom;
-
-  const [hovered, setHovered] = useState<InteractionData | null>(null);
-
-  // Scales
-  const yScale = d3.scaleLinear().domain([35, 85]).range([boundsHeight, 0]);
-  const xScale = d3
-    .scaleLinear()
-    .domain([-3000, 50000])
-    .range([0, boundsWidth]);
-  const allGroups = data.map((d) => String(d.group));
-  const colorScale = d3
-    .scaleOrdinal<string>()
-    .domain(allGroups)
-    .range(["#e0ac2b", "#e85252", "#6689c6", "#9a6fb0", "#a53253"]);
-
-    let filteredData: DataPoint[] = [];
-
-    if (state === 'europe') {
-      filteredData = data.filter(d => d.group === 'Europe');
-    } else if (state === 'asia') {
-      filteredData = data.filter(d => d.group === 'Asia');
-    } else if (state === 'africa') {
-      filteredData = data.filter(d => d.group === 'Africa');
-    } else if (state === 'americas') {
-      filteredData = data.filter(d => d.group === 'Americas');
-    } else if (state === 'oceania') {
-      filteredData = data.filter(d => d.group === 'Oceania');
-    }
-     else {
-      filteredData = data;
-    }
-
-    if (dating === 'small') {
-      filteredData = filteredData.filter(d => d.size < 19000000);
-    } else if (dating === 'medium') {
-      filteredData = filteredData.filter(d => d.size >= 19000000 && d.size < 100000000);
-    } else if (dating === 'large') {
-      filteredData = filteredData.filter(d => d.size >= 100000000);
-    }
-
-    console.log(filteredData);
-
-  const allShapes = filteredData.map((d, i) => {
-    return (
-      <circle
-        key={i}
-        r={8}
-        cx={xScale(d.x)}
-        cy={yScale(d.y)}
-        stroke={colorScale(d.group)}
-        fill={colorScale(d.group)}
-        fillOpacity={0.7}
-        onMouseEnter={() =>
-          setHovered({
-            xPos: xScale(d.x),
-            yPos: yScale(d.y),
-            name: d.subGroup,
-          })
-        }
-        onMouseLeave={() => setHovered(null)}
-      />
-    );
-  });
-
-  return (
-    <div style={{ position: "relative" }}>
-      <svg width={width} height={height}>
-        <g
-          width={boundsWidth}
-          height={boundsHeight}
-          transform={`translate(${[MARGIN.left, MARGIN.top].join(",")})`}
-        >
-
-          <AxisLeft yScale={yScale} pixelsPerTick={40} width={boundsWidth} />
-
-          <g transform={`translate(0, ${boundsHeight})`}>
-            <AxisBottom
-              xScale={xScale}
-              pixelsPerTick={40}
-              height={boundsHeight}
-            />
-          </g>
-          {allShapes}
-        </g>
-      </svg>
-
-      <div
-        style={{
-          width: boundsWidth,
-          height: boundsHeight,
-          position: "absolute",
-          top: 0,
-          left: 0,
-          pointerEvents: "none",
-          marginLeft: MARGIN.left,
-          marginTop: MARGIN.top,
-        }}
-      >
-        <Tooltip interactionData={hovered} />
-      </div>
-    </div>
-  );
-};
+export default ScatterPlot;
